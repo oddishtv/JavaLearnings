25b0a335820f30dbd955329d88f586bdbf6b45e7
oddishtv <undersmoker@gmail.com>
2015-04-20 18:12:36 +0300

    aa

--- src/com/company/Class18.java
+++ src/com/company/Class18.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 /**
  * Created by Vitalii_Oborskyi on 3/6/2015.
  */
@@ -26,19 +28,53 @@ public class Class18 {
         private boolean leap;
 
         public Year (int actualYearNumber){
-            this.actualYearNumber = actualYearNumber;
-            leap = this.isLeap(actualYearNumber);
+            this.actualYearNumber = this.isNotOld(actualYearNumber);
+            this.leap = this.isLeap(actualYearNumber);
+
+        }
+
+        private int isNotOld (int year){
+            int result = 0;
+
+            if (year >= 1582) {
+                result = year;
+            }
+
+            return result;
         }
 
         private boolean isLeap(int year){
+            boolean result = false;
+            if (year % 4 == 0) {
+                if ((year % 100 != 0) || (year % 400 == 0)) {
+                    result = true;
+                }
+            }
+            return result;
+        }
 
+        public int getYear(){
+            return this.actualYearNumber;
+        }
+        public boolean getLeap(){
+            return this.leap;
         }
 
     }
 
 
     public static void run() {
+        Scanner scanIn = new Scanner(System.in);
+    int ytemp;
+    Year y;
+        do {
+            System.out.println("Please enter a year more then >=1582");
+           ytemp = Integer.parseInt(scanIn.nextLine());
+            y = new Year(ytemp);
+         } while (y.getYear() == 0);
 
-
+        if (y.getLeap() == true){
+            System.out.println("The year " +y.getYear()+ " is leap");
+            } else System.out.println("The year " +y.getYear()+ " is not leap");
     }
 }
